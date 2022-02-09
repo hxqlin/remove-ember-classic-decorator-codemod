1ca0e8fc1c078ca2c2fd1518e57583f0bafd8edc
Hannah Lin <hlin@linkedin.com>
2022-02-08 17:31:46 -0800

    Basic implementation and test

---
+++ transforms/remove-ember-classic-decorator/__testfixtures__/basic.input.js
@@ -0,0 +1,4 @@
+import classic from 'ember-classic-decorator';
+
+@classic
+export default class MyComponent extends Component {}
--- transforms/remove-ember-classic-decorator/index.js
+++ transforms/remove-ember-classic-decorator/index.js
@@ -1,16 +1,36 @@
 const { getParser } = require('codemod-cli').jscodeshift;
-const { getOptions } = require('codemod-cli');
 
 module.exports = function transformer(file, api) {
   const j = getParser(api);
-  const options = getOptions();
+  const input = j(file.source);
 
-  return j(file.source)
-    .find(j.Identifier)
-    .forEach((path) => {
-      path.node.name = path.node.name.split('').reverse().join('');
+  // Remove the ember-classic-decorator import
+  input
+    .find(j.ImportDeclaration, {
+      source: {
+        value: 'ember-classic-decorator',
+      },
     })
-    .toSource();
+    .forEach((importDeclaration) => {
+      j(importDeclaration).remove();
+    });
+
+  // Remove the @classic decorator
+  input.find(j.ClassDeclaration).forEach((path) => {
+    if (path.value.decorators) {
+      // Check if the @classic decorator exists
+      const classicDecoratorIdx = path.value.decorators.findIndex(
+        (decorator) => decorator.expression.name === 'classic'
+      );
+      const hasClassicDecorator = classicDecoratorIdx > -1;
+
+      if (hasClassicDecorator) {
+        path.value.decorators.splice(classicDecoratorIdx, 1);
+      }
+    }
+  });
+
+  return input.toSource();
 };
 
 module.exports.type = 'js';
